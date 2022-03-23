6bc7f8e0fa5bda4ca010baa1792004a6e2b69696
mqthis12 <fildooogaming@gmail.com>
2022-03-23 18:18:59 +0100

    Standings

--- app/src/main/java/com/example/footx/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/footx/ui/home/HomeFragment.java
@@ -29,13 +29,6 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.title;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
         return root;
     }
